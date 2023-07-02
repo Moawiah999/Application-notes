34df95559a707197ab83b467c016757effd05b22
Moawiah999 <muawiyah454@gmail.com>
2023-07-02 16:42:21 +0300

    create note views and change theme app

---
+++ lib/views/notes_views.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotesViews extends StatefulWidget {
+  const NotesViews({super.key});
+
+  @override
+  State<NotesViews> createState() => _NotesViewsState();
+}
+
+class _NotesViewsState extends State<NotesViews> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
